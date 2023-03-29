38b253e0832139a7e48a14ab649e3ec68014f0e3
Andrea Monroy <andreamonroy@MacBook-Air-de-Andrea.local>
2023-03-29 13:59:08 -0500

    add stepId

--- apps/ledger-live-desktop/src/renderer/modals/SignClaiming/index.jsx
+++ apps/ledger-live-desktop/src/renderer/modals/SignClaiming/index.jsx
@@ -1,46 +1,46 @@
 import Modal from "~/renderer/components/Modal";
 
 const ClaimOperationModal = ({
-    t,
-    account,
-    parentAccount,
-    transaction,
-    status,
-    bridgeError,
-    bridgePending,
-    onRetry,
-    onTransactionSigned,
-    onTransactionError,
-    updateTransaction,
-    closeModal,
-    openedFromAccount,
-    openModal,
-    useApp,
-    ...props
+  t,
+  account,
+  parentAccount,
+  transaction,
+  status,
+  bridgeError,
+  bridgePending,
+  onRetry,
+  onTransactionSigned,
+  onTransactionError,
+  updateTransaction,
+  closeModal,
+  openedFromAccount,
+  openModal,
+  useApp,
+  ...props
 }) => {
-    console.log("ClaimOperationModal", props);
-    return (
-        <Modal
-            name="MODAL_SIGN_CLAIMING"
-            centered
-            refocusWhenChange={"device"}
-            onHide={handleReset}
-            preventBackdropClick
-            render={({ onClose, data }) => (
-                <Body
-                    stepId={"device"}
-                    onClose={() => {
-                        if (data.onCancel) {
-                            data.onCancel(new Error("Signature interrupted by user"));
-                        }
-                        onClose();
-                    }}
-                    onChangeStepId={handleStepChange}
-                    params={data || {}}
-                />
-            )}
+  console.log("ClaimOperationModal", props);
+  return (
+    <Modal
+      name="MODAL_SIGN_CLAIMING"
+      centered
+      refocusWhenChange={"device"}
+      onHide={closeModal ? () => closeModal() : undefined}
+      preventBackdropClick
+      render={({ onClose, data }) => (
+        <Body
+          stepId={"device"}
+          onClose={() => {
+            if (data.onCancel) {
+              data.onCancel(new Error("Signature interrupted by user"));
+            }
+            onClose();
+          }}
+          onChangeStepId={handleStepChange}
+          params={data || {}}
         />
-    );
-}
+      )}
+    />
+  );
+};
 
 export default ClaimOperationModal;
